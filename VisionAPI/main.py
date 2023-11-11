f6f056804ab3c4f455ce9ea19d71d11691c0420f
ericxu <ericxuhello3@gmail.com>
2023-11-10 16:01:15 -0800

    commit #3: added OCR functionality from Google Cloud Vision AI API

--- VisionAPI/main.py
+++ VisionAPI/main.py
@@ -26,11 +26,26 @@ def home():
         file = form.file.data
         # save the submitted file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
-        return "File has been uploaded."
+        # extract text from the image using Google Vision OCR API
+        image_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],'schedule.jpg')
+        with open(image_path, 'rb') as image_file:
+            content = image_file.read()
+        # credentials for google vision AI api
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+        # create a Google Vision Image object from the image content
+        image = vision.Image(content=content)
+        # create a Google Vision ImageAnnotatorClient object to access the OCR API
+        client = vision.ImageAnnotatorClient()
+        # send the image to the OCR API and get the response
+        response = client.text_detection(image=image)
+        # extract the text annotations from the response
+        texts = response.text_annotations
+        # get the first text annotation, which contains the extracted text
+        extracted_text = texts[0].description
+        # return the extracted text
+        return extracted_text
+    # if the form is not submitted or invalid, render the upload form
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
-client = vision.ImageAnnotatorClient()
